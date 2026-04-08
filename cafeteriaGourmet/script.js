671bea8b222b842d494417f9510df54749a4bce0
pedrooSoeiro <pr1004566@gmail.com>
2026-04-08 12:50:40 -0300

    cafeteria

---
+++ cafeteriaGourmet/script.js
@@ -0,0 +1,15 @@
+//Elabore um programa que peça o valor do pedido. Se o valor for maior que R$ 50,00, o sistema pergunta se o cliente tem o "Cartão Fidelidade":
+//Se Sim: Desconto de 10% no total.
+//Se Não: Ganha apenas um "Cookie de brinde".
+//Exiba a mensagem final com o valor a pagar e se houve brinde.
+
+let valorPedido = Number(prompt("Qual é o valor do seu pedido"))
+if (valorPedido >= 50){
+    let fidelidade = (prompt("Você tem o cartão fidelidade?"))
+        if (fidelidade === "sim" || fidelidade === "Sim"){
+        valorPedido = valorPedido * 0.90}
+        alert("O seu pedido com o cartão fidelidade ficou "+ valorPedido.toFixed(2))
+}
+else {
+    alert ("Pegue seu Cookie de brinde");}
+
